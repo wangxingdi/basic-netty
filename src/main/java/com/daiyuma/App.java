359abd42f5a9b64aac9dfdd0d9ec5e30cf11269d
wangxingdi <forwangxd@gmail.com>
2020-08-08 23:25:22 +0800

    'maven项目初始化'

---
+++ src/main/java/com/daiyuma/App.java
@@ -0,0 +1,13 @@
+package com.daiyuma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
